f80a747e7f2df86390369352550755739f29bc68
unknown <1804160133@D67A4.xwoda.sz>
2019-01-23 15:19:06 +0800

    myStyle提交

---
+++ myStyle/mainwindow.cpp
@@ -0,0 +1,25 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QWindowsXPStyle>
+#include<QPalette>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPalette palette1=ui->pushButton->palette();
+    palette1.setColor(QPalette::ButtonText,Qt::red);
+    palette1.setColor(QPalette::Button,Qt::green);
+    ui->pushButton->setPalette(palette1);
+    ui->lineEdit->setDisabled(true);
+
+    QPalette palette2=ui->lineEdit->palette();
+    palette2.setColor(QPalette::Disabled,QPalette::Base,Qt::blue);
+    ui->lineEdit->setPalette(palette2);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}

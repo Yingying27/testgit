f355158b8b1c6d44c074654cc7d75a239a9d40db
unknown <1804160133@D67A4.xwoda.sz>
2019-01-23 15:35:55 +0800

    颜色改变

--- myStyle/mainwindow.cpp
+++ myStyle/mainwindow.cpp
@@ -9,7 +9,7 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     QPalette palette1=ui->pushButton->palette();
-    palette1.setColor(QPalette::ButtonText,Qt::red);
+    palette1.setColor(QPalette::ButtonText,Qt::yellow);
     palette1.setColor(QPalette::Button,Qt::green);
     ui->pushButton->setPalette(palette1);
     ui->lineEdit->setDisabled(true);
@@ -17,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QPalette palette2=ui->lineEdit->palette();
     palette2.setColor(QPalette::Disabled,QPalette::Base,Qt::blue);
     ui->lineEdit->setPalette(palette2);//显示格式
+
 }
 
 MainWindow::~MainWindow()
